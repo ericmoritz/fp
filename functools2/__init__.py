6387e985950cdeeb5471cc0a81fdc03c35408573
Eric Moritz <eric@themoritzfamily.com>
2012-09-14 03:39:37 -0400

    Added a lot of what is in Haskell's core
    http://ww2.cs.mu.oz.au/172/Haskell/tourofprelude.html
    
    I added a getter() partial that simulates Clojure's
    
    (comp :foo :bar) composition

--- functools2/__init__.py
+++ functools2/__init__.py
@@ -3,18 +3,175 @@ functools2
 
 Provides a number of operator and higher-order functions for FP fun
 """
+
+####
+## Operators
+####
 from operator import (
-    add, concat, contains, truediv as div,
-    floordiv, and_, xor
+    # arithmatic
+    add, sub, mul, truediv as div, pow, mod, neg,
+
+    # bitwise ops
+    and_, xor, or_, invert, not_,
+
+    concat, contains,
+
+    # comparison
+    lt, le, eq, ne, gt, ge
 )
-from functools import partial as p
+import operator
+
+
+def quot(x,y):
+    """returns the quotient after dividing the its first integral
+    argument by its second integral argument"""
+    return operator.floordiv(x,y)
 
 
 def in_(item, container):
+
     """This mimics the signature of Haskell's `elem` function and the
 Python "in" operator."""
-    return contains(container, item)
+    return item in container
 
 
-# exports
-__all__ = [add, concat, p, div, floordiv, in_, and_, xor]
+def not_in(item, container):
+    return item not in container
+###
+# Partials
+###
+
+from functools import partial as p
+
+
+def c(f,g):
+    return lambda x: f(g(x))
+
+
+def const(x):
+    return lambda: x
+
+
+def flip(f):
+    return lambda x,y: f(y,x)
+
+
+def identity(x):
+    return x
+
+
+def getter(*args):
+    """Creates a function that digs into a nested data structure or returns
+default if any of the keys are missing
+
+    >>> addresses = [{"address": {"city": "Reston"}},
+    ...              {"address": {"city": "Herndon"}},
+    ...              {"address": {}}]
+    >>>
+    >>> get_city = getter("address", "city")
+    >>> get_cities = p(imap, get_city)
+    >>> list(get_cities(addresses))
+    ["Reston", "Herndon", None]
+    """
+
+    def inner(val):
+        if not args:
+            return None
+
+        ret = val
+        for key in args:
+            try:
+                ret = ret[key]
+            except (KeyError, IndexError):
+                return None
+        return ret
+    return inner
+
+###
+## lazy evaluating functions
+###
+from itertools import (
+    imap, ifilter, cycle as icycle, repeat as irepeat,
+    dropwhile as idropwhile, izip
+)
+
+
+def iand(iterable):
+    """takes the logical conjunction of a iterable of boolean values"""
+    # The built-in all function does the same thing as iand
+    return all(iterable)
+
+
+def iall(f, iterable):
+    """returns True if all elements of the list satisfy the predicate,
+    and False otherwise."""
+    return iand(imap(f, iterable))
+
+
+def ior(iterable):
+    """takes the logical disjunction of a iterable of boolean values"""
+    # The built-in any function does the same thing as ior
+    return any(iterable)
+
+
+def iany(f, iterable):
+    """returns True if any of the elements of the list satisfy the
+    predicate, and False otherwise"""
+    return ior(imap(f, iterable))
+
+
+def iadd(iterable):
+    return reduce(add, iterable)
+
+
+def iconcat_map(f, iterable):
+    return reduce(add, (imap(f, iterable)))
+
+
+def itake(n, iterable):
+    for item in iterable:
+        if n > 0:
+            yield item
+            n = n - 1
+        else:
+            return
+
+
+def itake_while(predicate, iterable):
+    for item in iterable:
+        if predicate(item):
+            yield item
+        else:
+            return
+
+
+def idrop(n, iterable):
+    for item in iterable:
+        if n > 0:
+            n = n - 1
+        else:
+            yield item
+
+
+def isplit_at(i, iterable):
+    yield itake(i, iterable)
+    yield iterable
+
+
+def izip_with(f, iterable1, iterable2):
+    return imap(f, iterable1, iterable2)
+
+
+
+####
+## Predicates
+####
+def even(x):
+    return mod(x, 2) == 0
+
+odd = c(not_, even)
+
+def is_none(x):
+    return x is None
+
+not_none = c(not_, is_none)
--- functools2/tests.py
+++ functools2/tests.py
@@ -50,11 +50,11 @@ class TestDiv(TestCase):
                           functools2.div(3, 2))
 
 
-class TestFloorDiv(TestCase):
+class TestQuot(TestCase):
 
     def test(self):
         self.assertEquals(1,
-                          functools2.floordiv(3, 2))
+                          functools2.quot(3, 2))
 
 
 class TestAnd(TestCase):
@@ -85,3 +85,225 @@ class TestXor(TestCase):
         self.assertIs(False,
                       functools2.xor(False, False))
 
+
+class TestPartial(TestCase):
+
+    def test(self):
+        add_three = functools2.p(functools2.add, 3)
+
+        self.assertEqual(6, add_three(3))
+
+
+class TestCompose(TestCase):
+
+    def test(self):
+        c = functools2.c
+        p = functools2.p
+        mul = functools2.mul
+        add = functools2.add
+
+        add_three_and_double1 = c(
+            lambda x: x * 2,
+
+            lambda x: x + 3)
+
+        add_three_and_double2 = c(p(mul, 2),
+                                  p(add, 3))
+
+        self.assertEqual(10,
+                         add_three_and_double1(2))
+
+        self.assertEqual(10,
+                         add_three_and_double2(2))
+
+
+class TestIAND(TestCase):
+    def test(self):
+        def crashing_gen():
+            yield True
+            yield False
+            raise Exception("crap")
+            yield True
+
+        self.assertFalse(functools2.iand(crashing_gen()))
+        self.assertTrue(functools2.iand([True, True, True]))
+
+
+class TestIAll(TestCase):
+
+    def test(self):
+        iall = functools2.iall
+        even = functools2.even
+        odd = functools2.odd
+
+        self.assertTrue(iall(even, xrange(2, 10, 2)))
+        self.assertFalse(iall(odd, xrange(2, 10, 2)))
+
+        # ensure that iall is lazy
+        def crashing_gen():
+            yield 2
+            yield 3
+            raise Exception("crap")
+            yield 4
+
+        # if this returns false, that means we never get to the
+        # exception in the generator
+        self.assertFalse(iall(even, crashing_gen()))
+
+
+class TestIOR(TestCase):
+
+    def test(self):
+        self.assertTrue(functools2.ior([True, False, True]))
+        self.assertFalse(functools2.ior([False, False, False]))
+
+        def crashing_gen():
+            yield False
+            yield True
+            raise Exception("crap")
+            yield False
+
+        self.assertTrue(functools2.ior(crashing_gen()))
+
+
+class TestIAny(TestCase):
+
+    def test(self):
+        iany = functools2.iany
+        even = functools2.even
+        odd = functools2.odd
+
+        self.assertTrue(iany(even, xrange(1, 10)))
+        self.assertFalse(iany(odd, xrange(2, 10, 2)))
+
+        # ensure that iall is lazy
+        def crashing_gen():
+            yield 2
+            raise Exception("crap")
+            yield 3
+            yield 4
+
+        # if this returns false, that means we never get to the
+        # exception in the generator
+        self.assertTrue(iany(even, crashing_gen()))
+
+
+class TestIAdd(TestCase):
+    def test(self):
+        iadd = functools2.iadd
+
+        self.assertEqual("eric",
+                         iadd(["er", "ic"]))
+
+        self.assertEqual([1, 2, 3, 4],
+                         iadd([[1, 2], [3, 4]]))
+
+
+class TestIConcatMap(TestCase):
+
+    def test(self):
+        iconcat_map = functools2.iconcat_map
+
+        self.assertEqual(
+            "1234",
+            iconcat_map(str, [1,2, 3,4]))
+
+
+class TestConst(TestCase):
+
+    def test(self):
+        one = functools2.const(1)
+
+        self.assertEqual(1, one())
+
+
+class TestDrop(TestCase):
+
+    def test(self):
+        self.assertEqual(
+            [3,4,5],
+            list(functools2.idrop(3, range(6))))
+
+
+class TestDropWhile(TestCase):
+
+    def test(self):
+        lt = functools2.lt
+        p = functools2.p
+        idrop_while = functools2.idropwhile
+
+        self.assertEqual(
+            [5, 6, 7, 8, 9, 10],
+            list(idrop_while(
+                lambda x: x < 5,
+                xrange(0, 11))))
+
+class TestTake(TestCase):
+
+    def test(self):
+        itake = functools2.itake
+
+        self.assertEqual(
+            [1,2,3],
+            list(
+                itake(3, xrange(1, 10))))
+
+
+class TestTakeWhile(TestCase):
+
+    def test(self):
+        itake_while = functools2.itake_while
+
+        def crashing_gen():
+            yield 1
+            yield 2
+            yield 3
+            yield 4
+            raise Exception("crap")
+            yield 5
+
+        self.assertEqual(
+            [1,2,3],
+            list(itake_while(
+                lambda x: x < 4,
+                crashing_gen())))
+
+
+class TestIZip(TestCase):
+
+    def test(self):
+        izip = functools2.izip
+
+        self.assertEqual(
+            [(1, 'a'),
+             (2, 'b'),
+             (3, 'c')],
+            list(izip([1, 2, 3], 'abc')))
+
+
+class TestZipWith(TestCase):
+
+    def test(self):
+        izip_with = functools2.izip_with
+        add = functools2.add
+
+        self.assertEqual(
+            [7, 9, 11, 13, 15],
+            list(izip_with(add,
+                           xrange(1, 6),
+                           xrange(6, 11))))
+
+
+class TestGetter(TestCase):
+
+    def test(self):
+        get_city = functools2.getter("addresses", 0, "city")
+
+        self.assertEqual("Reston",
+                         get_city({"addresses": [{"city": "Reston"}]}))
+
+        self.assertEqual(None,
+                         get_city({"addresses": []}))
+
+        self.assertEqual(None,
+                         get_city({"addresses": [{}]}))
