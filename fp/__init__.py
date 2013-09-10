053b97eb914a21b890d9c409caf31ffacee6873d
Eric Moritz <eric@themoritzfamily.com>
2013-09-10 17:46:45 -0400

    Added a better atom mechanism

--- fp/__init__.py
+++ fp/__init__.py
@@ -16,7 +16,14 @@ else:
 ####
 # atoms
 ####
-undefined = object()
+class atom(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+undefined = atom("undefined")
 
 ###
 # Higher-Order functions
